19344248145dac536e75812dc1623cfb40a0017c
Mustafa Dawoud <MustafaDawoud97@gmail.com>
2017-05-19 15:24:06 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace RS_232_SERIAL_COM_PORT_GUI
+{
+    partial class RS232
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoScroll = true;
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.label7, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(27, 25);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 547F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(336, 548);
+            this.tableLayoutPanel2.TabIndex = 1;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Voltage Average";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(230, 264);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Time";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(393, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(362, 548);
+            this.tableLayoutPanel1.TabIndex = 2;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint_1);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature Average";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label4, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(798, 25);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(351, 548);
+            this.tableLayoutPanel3.TabIndex = 3;
+            this.tableLayoutPanel3.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel3_Paint);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Current Average";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Time";
+            // 
+            // RS232
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1340, 618);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "RS232";
+            this.Text = "RS232";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace RS_232_SERIAL_COM_PORT_GUI
+{
+    public partial class RS232 : Form
+    {
+
+        // Create the serial port with basic settings
+        public SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
+        public RS232() 
+        {
+            InitializeComponent();
+
+            Console.WriteLine("Incoming Data:");
+
+            // Attach a method to be called when there
+            // is data waiting in the port's buffer
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            
+            try
+            {
+                // Begin communications
+                port.Open();
+
+            }catch(Exception)
+            {
+                // Enter an application loop to keep this thread alive
+                Application.Run();
+            }
+        }
+
+        private void port_DataReceived(object sender, ProgressChangedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            Console.WriteLine("Data Received:" + e.UserState);
+            String inputData = e.UserState.ToString;
+            TableLayoutPanel panel = tableLayoutPanel2;
+            TableLayoutPanel panel2 = tableLayoutPanel1;
+            TableLayoutPanel panel3 = tableLayoutPanel3;
+            String[] seperatedData = inputData.Trim().Split();
+            
+            //if (seperatedData.Length == 2)
+            //{
+                String id = seperatedData[0];
+            Console.WriteLine(id);
+            switch (id)
+                {
+                    case "0x00":
+                        try
+                        {
+                            double voltage = Convert.ToDouble(seperatedData[1]);
+                            DateTime now = DateTime.Now;
+                            RowStyle temp = panel.RowStyles[panel.RowCount - 1];
+
+                            //increase panel rows count by one
+                            panel.RowCount++;
+                            //add a new RowStyle as a copy of the previous one
+                            panel.RowStyles.Add(new RowStyle(temp.SizeType, temp.Height));
+                            panel.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+                            panel.Controls.Add(new Label() { Text = voltage.ToString() }, 0, panel.RowCount - 1);
+                            panel.Controls.Add(new Label() { Text = now.ToString() }, 1, panel.RowCount - 1);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.Error.WriteLine("Failed to read voltage(Not a number?)");
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Console.Error.WriteLine("Tried to draw to the screen after you removed it.");
+                        }
+                        break;
+                    case "0x01":
+                        try
+                        {
+                            double temperature = Convert.ToDouble(seperatedData[1]);
+                            DateTime now = DateTime.Now;
+                            RowStyle temp = panel2.RowStyles[panel.RowCount - 1];
+
+                            //increase panel rows count by one
+                            panel2.RowCount++;
+                            //add a new RowStyle as a copy of the previous one
+                            panel2.RowStyles.Add(new RowStyle(temp.SizeType, temp.Height));
+                            panel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+                            panel2.Controls.Add(new Label() { Text = temperature.ToString() }, 0, panel.RowCount - 1);
+                            panel2.Controls.Add(new Label() { Text = now.ToString() }, 1, panel.RowCount - 1);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.Error.WriteLine("Failed to read temperature(Not a number?)");
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Console.Error.WriteLine("Tried to draw to the screen after you removed it.");
+                        }
+                        break;
+                    case "0x02":
+                        try
+                        {
+                            double current = Convert.ToDouble(seperatedData[1]);
+                            DateTime now = DateTime.Now;
+                            RowStyle temp = panel.RowStyles[panel.RowCount - 1];
+
+                            //increase panel rows count by one
+                            panel.RowCount++;
+                            //add a new RowStyle as a copy of the previous one
+                            panel.RowStyles.Add(new RowStyle(temp.SizeType, temp.Height));
+                            panel.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+                            panel.Controls.Add(new Label() { Text = current.ToString() }, 0, panel.RowCount - 1);
+                            panel.Controls.Add(new Label() { Text = now.ToString() }, 1, panel.RowCount - 1);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.Error.WriteLine("Failed to read current(Not a number?)");
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Console.Error.WriteLine("Tried to draw to the screen after you removed it.");
+                        }
+                        break;
+                    }
+            //}
+            //else
+            //{
+              //  Console.Error.WriteLine("Input length incorrect (Might happen if we start reading in the wrong spot. No big deal)");
+            //}
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
